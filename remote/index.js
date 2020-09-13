26fb6f82e8f16ac080bfb89a1abc1378691ed8c8
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-09-13 19:26:10 +0530

    feat: poll the server

--- remote/index.js
+++ remote/index.js
@@ -1,4 +1,6 @@
 /* eslint-disable */
+const axios = require("axios")
+
 const all = () => {
   console.info("Get all users")
 }
@@ -11,8 +13,20 @@ const listen = (username, event, cb) => {
   console.info("Listen for specific event by specific user")
 }
 
+const poll = () => {
+  axios.post("http://localhost:41431/health")
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.log(err)
+      setTimeout(poll, 1430)
+    })
+}
+
 module.exports = {
   all,
+  poll,
   disconnect,
   listen
 }
