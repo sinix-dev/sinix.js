19ab1b3d61bb3d7c006681924bc03ab486a4b184
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-09-14 13:30:28 +0530

    websocket and event listener

--- remote/index.js
+++ remote/index.js
@@ -1,32 +1,66 @@
 /* eslint-disable */
 const axios = require("axios")
 
-const all = () => {
-  console.info("Get all users")
-}
+const EVENT_OBJ = {}
 
-const disconnect = (username) => {
-  console.info("Disconnect specific user")
-}
-
-const listen = (username, event, cb) => {
-  console.info("Listen for specific event by specific user")
-}
-
-const poll = () => {
+const poll = (resolve) => {
   axios.post("http://localhost:41431/health")
     .then((res) => {
-      console.log(res)
+
+      resolve()
     })
     .catch((err) => {
-      console.log(err)
-      setTimeout(poll, 1430)
+      setTimeout(() => {
+        poll(resolve)
+      }, 1430)
     })
+}
+
+const register = () => {
+  return new Promise((resolve, reject) => {
+    const params = new URLSearchParams()
+    params.append("username", "sinix")
+
+    axios.post("http://localhost:41431/register", params)
+      .then((res) => {
+        resolve(res)
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}
+
+const connect = () => {
+  const socket = new WebSocket("ws://localhost:41431/ws/sinix")
+
+  return socket
+}
+
+const setup = new Promise((resolve, reject) => {
+  poll(async () => {
+    await register()
+
+    const socket = connect()
+
+    socket.onmessage = (resp) => {
+      let { data } = resp
+
+      data = JSON.parse(data)
+
+      EVENT_OBJ[data.event_type](data.payload)
+    }
+
+    resolve(socket)
+  })
+})
+
+const listen = (evnt, cb) => {
+  setup.then((socket) => {
+    EVENT_OBJ[evnt] = cb
+  })
 }
 
 module.exports = {
-  all,
-  poll,
-  disconnect,
-  listen
+  listen,
 }
