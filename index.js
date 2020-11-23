f1b0283007efe3a6f4b3fc4eee10ca9f8004add2
Devansu-Yadav <devansuyadav@gmail.com>
2020-11-23 02:08:02 -0500

    Generated Documentation(JsDoc) for package

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+/**
+ * Loading "db" and "remote" modules
+ * @type {Object}
+ */
 const db = require("./db")
 const remote = require("./remote")
 
+/**
+ * Exporting "db" and "remote" modules
+ * @type {Object}
+ */
 module.exports = {
   db,
   remote
